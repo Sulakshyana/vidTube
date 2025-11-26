7bc11ba7d82d0d6da147e2c42eb0916b684ec3cb
swastika <swastikaghimire100@gmail.com>
2025-11-26 08:36:08 +0545

    feat(upload): configure multer and implement cloudinary upload with cleanup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,10 +16,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+app.use(cookieParser);
+
 //import routes
 import healthCheckRouter from "./routes/healthCheck.routes.js";
 
 //routes
-app.use("/api/v1/healthcheck", healthCheckRouter);
-
+app.use("/api/v1/health-check", healthCheckRouter);
 export { app };
